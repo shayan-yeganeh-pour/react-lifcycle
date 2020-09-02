2b60797664f8a14b78ac4d4dd4b2c9fcf641acc6
shayan-yeganeh-pour <shayan270@hotmail.com>
2020-09-02 19:00:44 +0430

    Persons.js has changed to the class-based

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { Component} from 'react'
 import Person from './Person/Person'
 
-const persons =  (props) => {
+class Persons extends Component {
+   render(){
        console.log("[App.js] rendering...")
-       return props.persons.map(( person, index ) => {
+       return this.props.persons.map(( person, index ) => {
         return (
          <Person
-            click={() => props.clicked(index)}
+            click={() => this.props.clicked(index)}
             name={person.name}
             age={person.age}
             key={person.id}
-            changed={event => props.changed(event, person.id)}
+            changed={event => this.props.changed(event, person.id)}
           />
         )
       })
     }
 
-
-export default persons;
+  }
+export default Persons;
